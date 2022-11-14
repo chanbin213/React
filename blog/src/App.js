c4ff7549802d674b570a26ad399087c51c10ff21
chanbin213 <pcb0213@gmail.com>
2022-11-14 18:47:55 +0900

    React Component 사용해서 div 줄이기

--- blog/src/App.js
+++ blog/src/App.js
@@ -33,8 +33,21 @@ function App(){
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal/>
+
     </div>
-  )
+  );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>    
+  );
 }
 
 export default App;
