2ad844aa63fe25b8f99e0e9ab7bcf2162c913c89
박찬빈 <pcb0213@gmail.com>
2022-11-14 10:14:59 +0900

    React 레이아웃용 JSX 사용해보기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+
+  let post = '우동 맛집'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
-  );
+  )
 }
 
 export default App;
