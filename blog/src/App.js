49d1962d1a113de3e8a142af13dd7b5b7fcd12c5
chanbin213 <pcb0213@gmail.com>
2022-11-16 18:46:15 +0900

    React class를 이용한 옛날 React 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,7 +2,7 @@
 
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 function App(){
  
@@ -50,6 +50,8 @@ function App(){
         modal == true ? <Modal color ={'gray'} 글제목 = {글제목} 글제목변경 ={글제목변경} title = {title}/> : null
       }
 
+      <Modal2/>
+
     </div>
   );
 }
@@ -66,6 +68,25 @@ function Modal(props){
         props.글제목변경(copy);}}>글 수정</button>
     </div>    
   );
+}
+
+class Modal2 extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      name :'kim',
+      age : 20
+    }
+  }
+  render(){
+    return(
+      <div>안녕 {this.state.age}
+        <button onClick={()=>{
+          this.setState({age : 21})
+        }}>버튼</button>
+        </div>
+    )
+  }
 }
 
 export default App;
