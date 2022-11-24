24627bfd1339f8ac05878f894bb1e7e0c9fe32b1
박찬빈 <pcb0213@gmail.com>
2022-11-24 13:51:45 +0900

    React import와 export 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './image/bg.png'
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
       <Navbar bg="dark" variant="dark">
         <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -25,18 +29,18 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>가격 : {shoes[0].price}원</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>가격 : {shoes[1].price}원</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>가격 : {shoes[2].price}원</p>
           </div>
         </div>
       </div>
